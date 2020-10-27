885c05f75eaffdde2c812bd2cb67ada7cae64641
Alexis Barba <alexbarba.2278@gmail.com>
2020-10-27 09:19:09 -0600

    first commit

---
+++ src/api/resolvers.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const mutations = require("./mutations");
+const queries = require("./queries");
+const types = require("./types");
+
+module.exports = {
+	Query: queries,
+	Mutation: mutations,
+	...types,
+};
